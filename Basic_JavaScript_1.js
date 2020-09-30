d4ba1ebdb2cd0094af7da330da5ad2a38dc752c1
elizabethfields <elifields22@gmail.com>
2020-09-29 18:20:06 -0600

    button
    button

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,11 @@
 <alert("Hello, World! I am an alert box!");
+<button onclick="My_First_Function()">
+		Click me!
+		</button>
+		<p id="Irish"></p>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+		}
