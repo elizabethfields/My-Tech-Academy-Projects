3fa8b3f68b52cc3793f9be0f812623bb0cd3b99c
elizabethfields <elifields22@gmail.com>
2020-09-30 22:35:35 -0600

    bold, italics, underline
    bold, italics, underline

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,8 +1,4 @@
 <alert("Hello, World! I am an alert box!");
-<button onclick="My_First_Function()">
-		Click me!
-		</button>
-		<p id="Irish"></p>
 	function My_First_Function() {
 		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
